b1fe8bf00a4829c9bd7e0a2354f6862f3d037d6d
onish-alex <onish.alex2000@gmail.com>
2021-01-19 15:56:20 +0200

    added threads hometask

---
+++ HomeTasks/Multithreading.Threads/LessonTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace MultithreadingTasks
+{
+    public class LessonTask
+    {
+        private decimal[] array;
+        private int arrayLength;
+        private int threadCount;
+        decimal[] threadResults;
+
+        public LessonTask(int arrayLength)
+        {
+            this.arrayLength = arrayLength;
+            this.array = new decimal[arrayLength];
+            var rand = new Random();
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = (decimal)rand.NextDouble();
+            }
+        }
+
+        public decimal Run(int threadCount)
+        {
+            this.threadCount = threadCount;
+            this.threadResults = new decimal[threadCount];
+            Thread[] threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threadResults[i] = 0;
+                threads[i] = new Thread((number) => GetPartialSum((int)number));
+                threads[i].Start(i);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+
+            return threadResults.Sum();
+        }
+
+        private void GetPartialSum(int number)
+        {
+            int start = number * (arrayLength / threadCount);
+            int end = (number + 1) * (arrayLength / threadCount);
+            for (int i = start; i < end; i++)
+            {
+                threadResults[number] += array[i];
+            }
+        }
+
+    }
+}
