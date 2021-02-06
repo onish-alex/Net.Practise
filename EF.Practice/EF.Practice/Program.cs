9090db341146022af068fd269a05b8175f4c0b14
onish-alex <onish.alex2000@gmail.com>
2021-02-06 19:30:18 +0200

    remove some debug lines

--- EF.Practice/EF.Practice/Program.cs
+++ EF.Practice/EF.Practice/Program.cs
@@ -1,22 +1,9 @@
 ﻿namespace EF.Practice
 {
-    using EF.Practice.CodeFirst.Contexts;
-    using EF.Practice.Repository;
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var db = new EmployeeDbContext();
-
-            var jc = new JobCandidateRepository(db);
-            var sp = new SalesPeopleRepository(db);
-            var shift = new ShiftsRepository(db);
-            var dep = new DepartmentsRepository(db);
-            var history = new EmployeePayHistoryRepository(db);
-            var emp = new EmployeeRepository(db);
-
-            var jc1 = jc.GetAll();
         }
     }
 }
